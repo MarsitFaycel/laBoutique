ab5108c859b17e01ac467c85e840e1205a315499
MarsitFaycel <marsfays@gmail.com>
2023-01-05 13:48:08 +0100

    product component

---
+++ src/main/front/laBoutique/src/app/product/product.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductComponent {}
