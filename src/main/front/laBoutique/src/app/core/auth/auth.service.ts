9f79eca1ec2c572405bf4c27a597e8f207fbd3a2
MarsitFaycel <marsfays@gmail.com>
2023-01-05 13:48:39 +0100

    service config

---
+++ src/main/front/laBoutique/src/app/core/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Login } from 'src/app/login/login.model';
+import { ApplicationConfigService } from '../config/application-config.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthServerProvider {
+  constructor(
+    private http: HttpClient,
+
+    private applicationConfigService: ApplicationConfigService
+  ) {}
+
+  login(credentials: Login): Observable<void> {
+    return this.http.post<any>(
+      this.applicationConfigService.getEndpointFor(
+        'http://localhost:8080/login'
+      ),
+      credentials
+    );
+  }
+
+  logout(): Observable<void> {
+    return new Observable((observer) => {
+      observer.complete();
+    });
+  }
+}
